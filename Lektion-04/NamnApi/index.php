5aa2d3aa4e934afcbc251808a3e622d55619f1cd
Mahmud Al Hakim <mahmud.alhakim@gmail.com>
2021-01-14 09:52:09 +0100

    Lektion 4

---
+++ Lektion-04/NamnApi/index.php
@@ -0,0 +1,22 @@
+<?php
+    
+    include 'App.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>10 Namn</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+</head>
+<body class="container">
+   
+    <h1>10 Namn från <a href="https://namnapi.se/">namnapi.se</a></h1>
+
+    <?php App::main(); ?>
+
+</body>
+</html>
