7962bb0c5d0be786c27ec0583c7d7ab7a9a82cbf
Mahmud Al Hakim <mahmud.alhakim@gmail.com>
2021-01-20 08:29:46 +0100

    NamnAPI V2

---
+++ Lektion-06/NamnAPI2/Name.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * En klass som beskriver ett namn
+ */
+class Name
+{
+    /**
+     * Instansvariabler
+     */
+    private $firstName;
+    private $lastName;
+    private $gender;
+    private $age;
+    private $email;
+
+    /**
+     * Konstruktor
+     */
+    public function __construct($firstName, $lastName, $gender)
+    {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->gender = $gender;
+        $this->age = rand(1, 100);
+        $this->email = $this->createEmail();
+    }
+
+    /**
+     * En metdod som genererar en epostadress
+     */
+    public function createEmail()
+    {
+        $email = $this->firstName . '.' . $this->lastName . '@example.com';
+        $email = mb_strtolower($email);
+
+        $search  = array('å', 'ä', 'ö', 'é', '-', ' ');
+        $replace = array('a', 'a', 'o', 'e', '', '');
+        $email = str_replace($search, $replace, $email);
+
+        return $email;
+    }
+
+    /**
+     * En metdod som konverterar ett objekt till en array
+     */
+    public function toArray()
+
+    {
+        $array = array(
+            "firstName" => $this->firstName,
+            "lastName" => $this->lastName,
+            "gender" => $this->gender,
+            "age" => $this->age,
+            "email" => $this->email
+        );
+
+        // print_r($array);
+        
+        return $array;
+    }
+}
---
+++ Lektion-06/NamnAPI2/index.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * NamnAPI V.2
+ * En egenutvecklad version av https://namnapi.se/
+ * 
+ * Date: 2021-01-20
+ * Copyright: MIT
+ * Contact: Mahmud Al Hakim
+ * https://github.com/mahmudalhakim/
+ */
+
+header("Content-Type: application/json; charset=UTF-8");
+
+
+include('Name.php');
+include('namesArrays.php');
+
+$limit = isset($_GET["limit"]) ? $_GET["limit"] : 10;
+
+$names = array();
+
+while (count($names) < $limit) {
+
+    if (rand(0, 1) == 0) {
+
+        $name = new Name(
+            $firstNamesMale[rand(0, count($firstNamesMale) - 1)],
+            $lastNames[rand(0, count($lastNames) - 1)],
+            'Male'
+        );
+
+    } else {
+
+        $name = new Name(
+            $firstNamesFemale[rand(0, count($firstNamesFemale) - 1)],
+            $lastNames[rand(0, count($lastNames) - 1)],
+            'Female'
+        );
+    }
+
+    // print_r($name->toArray());
+
+    array_push($names, $name->toArray());
+
+}
+
+shuffle($names);
+
+echo json_encode($names, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
